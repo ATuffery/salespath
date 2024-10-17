f8d9fc4a6440a2ed4179f34616bbfa7eea5bf169
FabienSerres <fabien.serres@iut-rodez.fr>
2024-10-17 17:24:10 +0200

    Base API

---
+++ SalesPathApp/src/main/java/fr/iutrodez/salespathapp/SalesPathAppApplication.java
@@ -0,0 +1,13 @@
+package fr.iutrodez.salespathapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPathAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesPathAppApplication.class, args);
+    }
+
+}
---
+++ SalesPathApp/src/test/java/fr/iutrodez/salespathapp/SalesPathAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iutrodez.salespathapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPathAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
