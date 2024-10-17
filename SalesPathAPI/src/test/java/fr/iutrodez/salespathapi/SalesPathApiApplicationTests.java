cbe8c8768594c37aa4c20391563aeafb21fc92fc
FabienSerres <fabien.serres@iut-rodez.fr>
2024-10-17 17:30:26 +0200

    Correction nom projet API

--- SalesPathAPI/src/test/java/fr/iutrodez/salespathapi/SalesPathApiApplicationTests.java
+++ SalesPathAPI/src/test/java/fr/iutrodez/salespathapi/SalesPathApiApplicationTests.java
@@ -1,10 +1,10 @@
-package fr.iutrodez.salespathapp;
+package fr.iutrodez.salespathapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SalesPathAppApplicationTests {
+class SalesPathApiApplicationTests {
 
     @Test
     void contextLoads() {
