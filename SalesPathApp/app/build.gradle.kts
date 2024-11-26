35b97ac4a5da46c910964b014b8d33e969a5e621
antoine.tuffery <antoine.tuffery@iut-rodez.fr>
2024-11-26 14:49:01 +0100

    UI page connexion OK

--- SalesPathApp/app/build.gradle.kts
+++ SalesPathApp/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "fr.iutrodez.salespathapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "fr.iutrodez.salespathapp"
