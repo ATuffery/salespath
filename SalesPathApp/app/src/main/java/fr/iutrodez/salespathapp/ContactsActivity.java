40e8abbaaef6e9acf38e8957b79069334f9ae4af
FabienSerres <fabien.serres@iut-rodez.fr>
2024-12-11 13:15:30 +0100

    Debut de la partie liste des compte

--- SalesPathApp/app/src/main/java/fr/iutrodez/salespathapp/ContactsActivity.java
+++ SalesPathApp/app/src/main/java/fr/iutrodez/salespathapp/ContactsActivity.java
@@ -7,15 +7,36 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ContactsActivity extends BaseActivity  {
+
+    private RequestQueue queue;
+    private Intent intent;
+    private static final String URL = "http://ec2-13-39-14-30.eu-west-3.compute.amazonaws.com:8080/client/get?id=";
+
+    private String id;
+    private String apiKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,36 +44,75 @@ public class ContactsActivity extends BaseActivity  {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerViewHorizontal);
 
-        List<CardWithTwoLines> itineraryList = Arrays.asList(
-                new CardWithTwoLines(
-                        "Lohan Vignals",
-                        "Client",
-                        "25 Avenue de Bordeaux 12000 Rodez",
-                        "Pierre Fabre",
-                        "Voir détails",
-                        () -> {
-                            // Action à exécuter au clic sur le bouton
-                            Log.d("Lohan", "Lohan");
-                        }
-                ),
-                new CardWithTwoLines(
-                        "Antoine Tuffery",
-                        "Prospect",
-                        "25 Avenue de Bordeaux 12000 Rodez",
-                        "Doxallia",
-                        "Voir détails",
-                        () -> {
-                            // Autre action
-                            Log.d("Antoine", "Antoine");
-                        }
-                )
-        );
+        this.intent = getIntent();
+        this.id = intent.getStringExtra("accountId");
+        this.apiKey = intent.getStringExtra("apiKey");
 
+       this. queue = Volley.newRequestQueue(this);
 
-        CardWithTwoLinesAdapteur adapter = new CardWithTwoLinesAdapteur(itineraryList);
+        List<CardWithTwoLines> clientList = getClientList();
+
+        CardWithTwoLinesAdapteur adapter = new CardWithTwoLinesAdapteur(clientList);
         recyclerView.setAdapter(adapter);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private List<CardWithTwoLines> getClientList() {
+        this.queue.add(requestClientList(this.URL + this.id));
+        return null;
+    }
+
+    /**
+     * Créer une requête Volley pour récupérer la liste des clients d'un commercial avec l'API
+     * @param url l'url de l'API
+     * @return le JSON avec les informations du compte en cas de succès
+     */
+    private JsonObjectRequest requestClientList(String url) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String lastName = response.getString("lastName");
+                            String firstName = response.getString("firstName");
+
+                            lastNameInput.setText(lastName);
+                            firstNameInput.setText(firstName);
+                            addressInput.setText(response.getString("address"));
+                            emailInput.setText(response.getString("email"));
+
+                            updateInitials(firstName, lastName);
+                        } catch (JSONException e) {
+                            displayServerError();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse != null) {
+                            int statusCode = error.networkResponse.statusCode;
+                            if (statusCode == 404) {
+                                Toast.makeText(getBaseContext(), R.string.error_find_account,
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                displayServerError();
+                            }
+                        } else {
+                            displayServerError();
+                        }
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put("X-API-KEY", intent.getStringExtra("apiKey"));
+                return headers;
+            }
+        };
+        return jsonObjectRequest;
     }
 
     public void goToCreateContact(View btn) {
