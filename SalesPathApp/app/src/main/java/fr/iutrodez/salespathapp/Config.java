e78942ddd7be6c4291dc8c509590e3c08554b1da
antoine.tuffery <antoine.tuffery@iut-rodez.fr>
2025-01-23 09:33:01 +0100

    FIX API URL

--- SalesPathApp/app/src/main/java/fr/iutrodez/salespathapp/Config.java
+++ SalesPathApp/app/src/main/java/fr/iutrodez/salespathapp/Config.java
@@ -2,6 +2,6 @@ package fr.iutrodez.salespathapp;
 
 public class Config {
 
-    public static final String API_URL = "http://ec2-52-47-174-172.eu-west-3.compute.amazonaws.com/";
+    public static final String API_URL = "http://ec2-52-47-174-172.eu-west-3.compute.amazonaws.com:8080/";
 
 }
